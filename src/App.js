793fcfa2a95bf571a1c0918fdf511cdc853c60e4
Zehong Li <dominiclesnar233@gmail.com>
2023-02-18 16:18:08 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import about from '../src/img/about.png'
+import contact from '../src/img/contact.png'
+import music from '../src/img/music.png'
+import projects from '../src/img/projects.png'
+import resume from '../src/img/resume.png'
+
+
+import './index.css';
+
+//need a CV link page
+//need spotify link page
+//github display
+
+const App = () => {
+  const [open, setOpen] = useState(false)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hi, I'm Dominik, also known as ONIROZ</h1>
+      <div className = 'menu-container'>
+        <div className = 'menu-trigger' onClick = {() => {setOpen(!open)}}>
+          <button className='who-am-i'> Who am I? </button>
+        </div>
+
+        <div className = {`dropdown-menu ${open? 'active' : 'inactive'}`} >
+          <h3> Zehong Li (Dominik / ONIROZ) <br/> <span> UCSD Undergrad / Software Engineer / Web Designer/ Music Producer </span> </h3>
+          <ul>
+            <DropdownItem img = {about} text = {"My Info"}/>
+            <DropdownItem img = {projects} text = {"My Projects"}/>
+            <DropdownItem img = {music} text = {"My Music"}/>
+            <DropdownItem img = {resume} text = {"My Resume"}/>
+            <DropdownItem img = {contact} text = {"My Contact"}/>
+          </ul>
+        </div>
+      </div>
     </div>
+  );
+};
+
+function DropdownItem(props){
+  return(
+    <li className='dropdownItem'>
+      <img src = {props.img}></img>
+      <a> {props.text} </a>
+    </li>
   );
 }
 
